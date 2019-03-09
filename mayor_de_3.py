215a78d78f07db398515c5a5c39bf07b3f35c026
SantiM <smt093@gmail.com>
2019-03-09 13:38:39 -0300

    Aplicando format

---
+++ mayor_de_3.py
@@ -0,0 +1,6 @@
+numero_uno = int(input("Dime un numero: "))
+numero_dos = int(input("Dime un numero: "))
+numero_tres = int(input("Dime un numero: "))
+mayor = int(max(numero_uno, numero_dos, numero_tres))
+
+print("El numero más grande de los ingresados es {}".format(mayor))
