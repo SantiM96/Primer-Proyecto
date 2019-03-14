bc73c99f09bb4b13fe4d369c24334eaa9f0aab00
SantiM <smt093@gmail.com>
2019-03-14 19:35:34 -0300

    conversor celsius - farenheit terminado (sin retornos)

--- conversor Fº - Cº.py
+++ conversor Fº - Cº.py
@@ -1,23 +1,23 @@
 
-result = 0
-
 print("")
 print("Bienvenido al conversor de unidades entre Celsius y Farenheit")
 print("")
 
 unity_1 = input("Ingrese la unidad que desea cambiar( C / F ): ").upper()
+
 if unity_1 == "C":
     unity_2 = "F"
 elif unity_1 == "F":
     unity_2 = "C"
-else:
-    print("Ingrese un valor de los indicados( C / F )")
 
 if unity_1 == "C":
     value_1 = float(input("Ingrese el valor en grados Celsius a convertir: "))
     result = 1.8 * value_1 + 32
-    print(" grados Celsius equivale a {0:.2f} ".format(result))
+    print("")
+    print(value_1, "º Celsius equivalen a {0:.2f}º Farenheit".format(result))
 elif unity_1 == "F":
     value_1 = float(input("Ingrese el valor en grados Farenheit a convertir: "))
     result = (value_1 - 32)/1.8
-
+    print("")
+    print(value_1, "º Farenheit equivalen a {0:.2f}º Celsius".format(result))
+print("")
