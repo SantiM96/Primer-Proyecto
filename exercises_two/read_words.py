8a97f2811f3ca3dc6a68b615e1d456f59ca01977
SantiM <smt093@gmail.com>
2019-03-25 20:39:23 -0300

    mod strings

---
+++ exercises_two/read_words.py
@@ -0,0 +1,34 @@
+
+print("")
+txt_usu = input("Ingrese una oración: ")
+print("")
+new_str = ""
+quan = dict()
+index = 1
+more = 0
+
+#apart word
+for letter in txt_usu:
+    if letter != " ":
+        new_str += letter
+    if letter == " " or index == len(txt_usu):
+        if new_str in quan:
+            more = quan[new_str]
+            more += 1
+            quan[new_str] = more
+        if new_str not in quan:
+            quan[new_str] = 1
+        new_str = ""
+    index += 1
+
+print("Usted ha ingresado las siguientes palabras")
+
+#print words
+for word in quan:
+    forprint = word
+    word += ":"
+    if quan[forprint] == 1 or quan[forprint] == -1:
+        print(word, quan[forprint], "vez")
+    else:
+        print(word, quan[forprint], "veces")
+print("")
