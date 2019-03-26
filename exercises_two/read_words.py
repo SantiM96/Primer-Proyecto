a99cabaa18f4e50e940cb1698e48a1b4fc54d5fc
SantiM <smt093@gmail.com>
2019-03-25 21:09:36 -0300

    , . ! ¡ ? ¿

--- exercises_two/read_words.py
+++ exercises_two/read_words.py
@@ -9,7 +9,7 @@ more = 0
 
 #apart word
 for letter in txt_usu:
-    if letter != " ":
+    if letter != " " and letter != "." and letter != "," and letter != "!"and letter != "¡" and letter != "?" and letter != "¿":
         new_str += letter
     if letter == " " or index == len(txt_usu):
         if new_str in quan:
@@ -21,7 +21,9 @@ for letter in txt_usu:
         new_str = ""
     index += 1
 
-print("Usted ha ingresado las siguientes palabras")
+print("")
+print("Usted ha ingresado las siguientes palabras:")
+print("")
 
 #print words
 for word in quan:
