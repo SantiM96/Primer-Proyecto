8a389b65d0a83bf685390191d9d6d8999b9df96a
SantiM <smt093@gmail.com>
2019-04-22 21:10:57 -0300

    Modelo terminado, falta ajustar cambio de meses

--- exercises_two/date_time/death_date.py
+++ exercises_two/date_time/death_date.py
@@ -1,7 +1,7 @@
 import datetime, random
 
 #It has to be divisible by 1.5
-MID_LIFE = 80
+MID_LIFE = 90
 
 TOTAL_PENALIZATION_SMOKE = 8
 TOTAL_PENALIZATION_DRINK = 6
@@ -29,27 +29,42 @@ def discount(PENALIZATION, message):
     return int(discount)
 
 print("")
-print("¡Averigua la fecha de tu muerte!")
-print("--------------------------------")
+print("     ¡Averigua la fecha de tu muerte!")
+print("-------------------------------------------")
 print("Ingresa tu fecha de nacimiento [dd/mm/aaaa]")
+birth_date = input()
+birth_date = datetime.datetime.strptime(birth_date, "%d/%m/%Y")
 
+print("")
 disc_smoke = discount(TOTAL_PENALIZATION_SMOKE, "¿Fumas?")
 disc_drink = discount(TOTAL_PENALIZATION_DRINK, "¿Bebes?")
 disc_run = discount(TOTAL_PENALIZATION_RUN, "¿Haces ejercicio?")
 
+
 discount_life = disc_smoke + disc_drink - disc_run
 life = random.randint(int(MID_LIFE/1.5), MID_LIFE)
 death_years_old = life - discount_life
+death_days_old = death_years_old * 365
 
-print(death_years_old)
+# Extra to vary the months between birth and death
+if disc_smoke != 0:
+    death_days_old += 47
+if disc_drink != 0:
+    death_days_old += 54
+if disc_run != 0:
+    death_days_old += 23
 
+# Dates
+death_day = birth_date + datetime.timedelta(days=death_days_old)
+rest_year_alive = death_day - datetime.datetime.now()
+rest_year_alive = int(rest_year_alive.total_seconds() / 31536000)
 
+if rest_year_alive > 0:
+    print("")
+    print(death_day.strftime("Te vas a morir a los {} años de edad, el %d del %m del %Y".format(death_years_old)))
+    print("Disfruta tus últimos {} años de vida".format(rest_year_alive))
+    print("")
+else:
+    print("")
+    print("Ups, eres afortunado, deberías estar muerto")
 
-
-
-
-
-
-
-
-print("Morirás el")
