a7affd14f1b6454d66dc26886d0b931984c560e5
SantiM <smt093@gmail.com>
2019-05-17 20:00:40 -0300

    vesión final

--- exercises_two/height.py
+++ exercises_two/height.py
@@ -6,78 +6,93 @@ De una lista de N cantidad de estaturas de personas se desea saber lo siguiente
 4) Cuantos son mayores a 1.70
 """
 
-def check_number_height(number, text):
+def check_number_height(number, min_number, max_number, text_error):
     list_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    not_is_number = True
-    is_possible_height = True
-    while not_is_number and is_possible_height:
-        not_is_number = True
-        is_possible_height = True
-        while not_is_number:
-            list_usu = []
-            for item in number:
-                list_usu.append(item)
-            if len(list_usu) == 4:
-                if list_usu[0] in list_numbers and list_usu[1] == "." and\
-                   list_usu[2] in list_numbers and list_usu[3] in list_numbers:
-                    not_is_number = False
-                else:
-                    number = input(text)
-            elif len(list_usu) == 3:
-                if list_usu[0] in list_numbers and list_usu[1] == "." and list_usu[2] in list_numbers:
-                    not_is_number = False
-                else:
-                    number = input(text)
-            else:
-                number = input(text)
+    condition_while = True
 
-            if not not_is_number:
-                number = float(number)
-                if number < 3.5 and number > 1.0:
-                    is_possible_height = False
-                else:
-                    number = input(text)
+    while condition_while:
+        condition_one = False
+        condition_two = False
+        list_of_number = []
+        for item in number:
+            list_of_number.append(item)
 
+
+        if len(list_of_number) == 1:
+            if list_of_number[0] in list_numbers:
+                condition_one = True
+
+
+        elif len(list_of_number) == 3:
+            if list_of_number[0] in list_numbers and list_of_number[1] == "." and list_of_number[2] in list_numbers:
+                condition_one = True
+
+
+        elif len(list_of_number) == 4:
+            if list_of_number[0] in list_numbers and list_of_number[1] == "." and\
+            list_of_number[2] in list_numbers and list_of_number[3] in list_numbers:
+                condition_one = True
+
+
+        if condition_one:
+            number = float(number)
+            if number <= max_number and number >= min_number:
+                condition_two = True
+
+
+        if condition_one and condition_two:
+            condition_while = False
+        else:
+            number = input(text_error)
 
     return number
-def check_number_height_key(number, text, keyword):
+def check_number_height_key(number, min_number, max_number, key, text_error):
     list_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    not_is_number = True
-    is_possible_height = True
-    while not_is_number or is_possible_height:
-        not_is_number = True
-        is_possible_height = True
-        while not_is_number:
-            list_usu = []
-            for item in number:
-                list_usu.append(item)
-            if len(list_usu) == 4:
-                if list_usu[0] in list_numbers and list_usu[1] == "." and\
-                   list_usu[2] in list_numbers and list_usu[3] in list_numbers:
-                    not_is_number = False
-                else:
-                    number = input(text)
-            elif len(list_usu) == 3:
-                if list_usu[0] in list_numbers and list_usu[1] == "." and list_usu[2] in list_numbers:
-                    not_is_number = False
-                else:
-                    number = input(text)
+    condition_while = True
+
+    while condition_while:
+        condition_one = False
+        condition_two = False
+        list_of_number = []
+
+        for item in number:
+            list_of_number.append(item)
+
+
+        if len(list_of_number) == 1:
+            if list_of_number[0] in list_numbers:
+                condition_one = True
+
+
+        elif len(list_of_number) == 3:
+            if list_of_number[0] in list_numbers and list_of_number[1] == "." and list_of_number[2] in list_numbers:
+                condition_one = True
+
+
+        elif len(list_of_number) == 4:
+            if list_of_number[0] in list_numbers and list_of_number[1] == "." and\
+            list_of_number[2] in list_numbers and list_of_number[3] in list_numbers:
+                condition_one = True
+
+
+        if condition_one:
+            number = float(number)
+            if number <= max_number and number >= min_number:
+                condition_two = True
+
+
+        if (condition_one and condition_two):
+            condition_while = False
+        elif not condition_one:
+            if number.lower() == key:
+                condition_while = False
             else:
-                number = input(text)
-
-            if not not_is_number:
-                number = float(number)
-                if number < 3.5 and number > 1.0:
-                    is_possible_height = False
-                else:
-                    number = input(text)
-            if keyword == number:
-                not_is_number = False
-                is_possible_height = False
-
-
+                number = input(text_error)
+        else:
+            number = input(text_error)
 
     return number
+
 
 list_height = []
 higher = []
@@ -87,20 +102,23 @@ mid_point = 0
 
 
 height = input("\nIngrese su estatura: ")
-height = check_number_height(height, "Ingrese una estatura válida: ")
+height = check_number_height(height, 1, 3.5, "Ingrese una estatura válida: ")
 list_height.append(height)
 height = input("Ingrese su estatura: ")
-height = check_number_height(height, "Ingrese una estatura válida; ")
+height = check_number_height(height, 1, 3.5, "Ingrese una estatura válida: ")
 list_height.append(height)
 
-condition = True
-while condition:
+
+
+main_condition = True
+while main_condition:
     height = input("Ingrese su estatura o ingrese listo para continuar: ")
-    if height.lower() != "listo":
-        height = check_number_height_key(height, "Ingrese una estatura válida: ", "listo")
-        list_height.append(height)
+    height = check_number_height_key(height, 1, 3.5, "listo", "Ingrese una estatura válida: ")
+    if height == "listo":
+        main_condition = False
     else:
-        condition = False
+        list_height.append(height)
+
 
 # Average
 for number in list_height:
@@ -120,8 +138,8 @@ for number in list_height:
 for number in list_height:
     if number < 1.70:
         less.append(number)
-
-print("\n\nEl promedio de las estaturas es: {}"
+print("\n------------------------------------------------------")
+print("\nEl promedio de las estaturas es: {}"
       "\nEl punto medio es: {}"
       "\nLos mayores a 1.70 son: {}"
       "\nLos menores a 1.70 son: {}".format(average, mid_point, higher, less))
